82444c6fc514b144b6daa03161e70f38ad33f6a4
= <=>
2023-08-03 17:26:27 +0000

    with style incoporated

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Solving N Queens with Backtracking"""
 
+
 import sys
+
 
 def is_safe_to_place_queen(row, col, placed_queens):
     """
@@ -9,31 +11,40 @@ def is_safe_to_place_queen(row, col, placed_queens):
     Returns True if there are no conflicts with previously placed queens.
     """
     for queen_row, queen_col in placed_queens:
-        if col == queen_col or row - col == queen_row - queen_col or col - queen_col == queen_row - row:
+        if col == queen_col or\
+                row - col == queen_row - queen_col or\
+                col - queen_col == queen_row - row:
             return False
     return True
+
 
 def solve_nqueens(board_size, current_row, placed_queens):
     """
     Solve the N-Queens problem by placing queens on an N by N board so that
     no queens are attacking each other.
     Parameters:
-        board_size: An integer that sets the board size and the number of queens.
+        board_size: An integer that sets the board size
+                    and the number of queens.
         current_row: The current row being considered for queen placement.
-        placed_queens: The current state of the board represented as a list of queen positions.
+        placed_queens: The current state of the board
+                    represented as a list of queen positions.
     """
     if current_row == board_size:
-        # If we have placed queens in all rows (reached the end of the board), print the solution.
+        # If we have placed queens in all rows (reached the end of the board)
+        # print the solution.
         print(placed_queens)
     else:
         for col in range(board_size):
             if is_safe_to_place_queen(current_row, col, placed_queens):
-                # If it's safe to place a queen at position (current_row, col), add it to the board.
+                # If it's safe to place a queen at position (current_row, col)
+                # add it to the board.
                 placed_queens.append((current_row, col))
                 # Proceed to the next row by making a recursive call.
                 solve_nqueens(board_size, current_row + 1, placed_queens)
-                # Backtrack and remove the last queen placement to explore other possibilities.
+                # Backtrack and remove the last queen placement
+                # to explore other possibilities.
                 placed_queens.pop()
+
 
 def main():
     if len(sys.argv) != 2:
@@ -51,6 +62,7 @@ def main():
         sys.exit(1)
 
     solve_nqueens(board_size, 0, [])
+
 
 if __name__ == '__main__':
     main()
